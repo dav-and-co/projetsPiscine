441104f798d07facdde34f21342566c640a642e7
dav-and-co <claude.daviatte@lapiscine.pro>
2024-07-04 14:22:16 +0200

    le header

---
+++ headerGP.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRESUS Gironde</title>
+    <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-icon">
+    <meta name="description" content="Mission reconnue d'utilité publique. Accompagnement budgétaire des personnes en difficultés financières - Lutte contre le surendettement - Micro-crédit."/>
+    <meta name="author" content="Daviatte Claude"/>
+    <link rel="stylesheet" href="../assets/stylecopy.css">
+    <!-- bibliothèque fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header>
+        <!-- création de la nav commune au site gd public -->
+        <nav>
+            <ul> 
+                <li><a href="#top"> <img src="../assets/img/CRESUS-logo.png" alt="logo de l association crésus"></a></li>
+                <li><a href="#nous">Qui sommes nous</a></li>
+                <li><a href="#nousTrouver">Nous contacter</a></li>
+                <li><a href=" http://localhost/projet_cd/views/action.php">Nos actions</a></li>
+                <li><a href="#Vous aider">Vous aider</a></li>
+                <li><a href="#connexion">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
